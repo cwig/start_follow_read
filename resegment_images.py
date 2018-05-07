874ac3875f07364d89d0da2b7fdd09964ff076c4
Curtis Wigington <cwig5945@gmail.com>
2018-05-07 15:10:52 -0600

    batched resegment

--- resegment_images.py
+++ resegment_images.py
@@ -9,6 +9,8 @@ from e2e.alignment_dataset import AlignmentDataset
 import numpy as np
 from utils import transformation_utils
 import cv2
+import time
+from collections import defaultdict
 
 def main():
 
@@ -17,12 +19,14 @@ def main():
     with open(config_path) as f:
         config = yaml.load(f)
 
-    for dataset_lookup in ['training_set', 'validation_set']:
+    for dataset_lookup in ['training_set']:
+    # for dataset_lookup in ['training_set', 'validation_set']:
         set_list = load_file_list(config['training'][dataset_lookup])
         dataset = AlignmentDataset(set_list, None)
         dataloader = DataLoader(dataset, batch_size=1, shuffle=False, num_workers=0, collate_fn=alignment_dataset.collate)
 
-        output_grid_size = config['network']['hw']['input_height']
+        output_grid_size = 60
+        # output_grid_size = config['network']['hw']['input_height']
         t = ((np.arange(output_grid_size) + 0.5) / float(output_grid_size))[:,None].astype(np.float32)
         t = np.repeat(t,axis=1, repeats=output_grid_size)
         t = Variable(torch.from_numpy(t), requires_grad=False, volatile=True)
@@ -46,16 +50,18 @@ def main():
                 img_np = ((img_np+1)*128).astype(np.uint8)
                 img_np = img_np.copy()
 
-                renorm_matrix = transformation_utils.compute_renorm_matrix(img)
-                expanded_renorm_matrix = renorm_matrix.expand(1,3,3)
+                renorm_matrix = transformation_utils.compute_renorm_matrix(img)[None,...]
+                # expanded_renorm_matrix = renorm_matrix.expand(1,3,3)
 
                 print l_i['img_key']
 
-                for item in l_i['gt_json']:
+                all_lf_paths = defaultdict(list)
+                for j, item in enumerate(l_i['gt_json']):
 
                     if 'lf' not in item or 'hw_path' not in item:
                         continue
 
+                    start = time.time()
                     hw_path = item['hw_path']
 
                     lf_pts = item['lf']
@@ -74,13 +80,29 @@ def main():
                         pt = Variable(pt, requires_grad=False, volatile=True)
                         lf_path.append(pt)
 
+                    all_lf_paths[len(lf_path)].append((lf_path, hw_path))
+
+                for pairs in all_lf_paths.values():
+                    lf_paths = [p[0] for p in pairs]
+                    hw_paths = [p[1] for p in pairs]
+
+                    to_join = [[] for i in range(len(lf_paths))]
+                    for lf_path in lf_paths:
+                        for i in xrange(len(lf_path)):
+                            to_join[i].append(lf_path[i])
+
+                    for i in xrange(len(to_join)):
+                        to_join[i] = torch.cat(to_join[i], dim=0)
+
+                    lf_path = to_join
+
                     grid_line = []
                     for i in xrange(0, len(lf_path)-1):
                         pts_0 = lf_path[i]
                         pts_1 = lf_path[i+1]
                         pts = torch.cat([pts_0, pts_1], dim=2)
 
-                        grid_pts = expanded_renorm_matrix.bmm(pts)
+                        grid_pts = renorm_matrix.matmul(pts)
 
                         grid = interpolations[None,:,:,None,:] * grid_pts[:,None,None,:,:]
                         grid = grid.sum(dim=-1)[...,:2]
@@ -89,14 +111,18 @@ def main():
 
                     grid_line = torch.cat(grid_line, dim=1)
 
-                    line = torch.nn.functional.grid_sample(img.transpose(2,3), grid_line)
+                    expand_img = img.expand(grid_line.size(0), img.size(1), img.size(2), img.size(3))
+
+
+                    line = torch.nn.functional.grid_sample(expand_img.transpose(2,3), grid_line)
                     line = line.transpose(2,3)
 
-                    line = line[0].transpose(0,1).transpose(1,2)
-                    line = (line + 1)*128
-                    l_np = line.data.cpu().numpy()
+                    for line_i, line_i_path in zip(line, hw_paths):
+                        line_i = line_i.transpose(0,1).transpose(1,2)
+                        line_i = (line_i + 1)*128
+                        l_np = line_i.data.cpu().numpy()
 
-                    cv2.imwrite(hw_path, l_np)
+                        cv2.imwrite(line_i_path, l_np)
 
 if __name__ == "__main__":
     main()
